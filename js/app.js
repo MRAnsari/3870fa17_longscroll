268d9e3e228a7c2d44675f5f581dd36b2ab80d74
MRAnsari <artstudent@Arts-194-20.local>
2017-11-30 16:54:34 -0800

    Fixing the ease in

--- js/app.js
+++ js/app.js
@@ -1,53 +1,58 @@
 var canvas = document.getElementById("canvas");
 var counter = document.getElementById("counter");
-var icon = document.getElementById("icon");
+var poem = document.getElementById("poem");
 
 window.onscroll = function(){
   var y = window.pageYOffset;
 
   console.log(y);
 
-    poem.style.top = 150 + y * 1 + "px";
+    title.style.top = 400 + y * .7 + "px";
+    // poem.style.top = 150 + y * 1 + "px";
+    // poem.style.top = 500 + y * .5 + "px";
+
+    if (y > 0 && y < 1200) {poem.style.top = 500 + y * .7 + "px";}
+    else                    poem.style.top = 150 + y + "px";
 
 // POEM HILIGHTING
-    if (y > 1000 && y < 2000)   {line01.style.color = "red";}
-    else                        {line01.style.color = "black";}
-    if (y > 2000 && y < 3000)   {line02.style.color = "red";}
-    else                        {line02.style.color = "black";}
-    if (y > 3000 && y < 4000)   {line03.style.color = "red";}
-    else                        {line03.style.color = "black";}
-    if (y > 4000 && y < 5000)   {line04.style.color = "red";}
-    else                        {line04.style.color = "black";}
-    if (y > 5000 && y < 6000)   {line05.style.color = "red";}
-    else                        {line05.style.color = "black";}
-    if (y > 6000 && y < 7000)   {line06.style.color = "red";}
-    else                        {line06.style.color = "black";}
+    if (y > 2000 && y < 3000)   {line01.style.color = "rgba(255, 247, 97, 1)";}
+    else                        {line01.style.color = "rgba(255, 247, 97, 0.1)";}
+    if (y > 3000 && y < 4000)   {line02.style.color = "rgba(255, 247, 97, 1)";}
+    else                        {line02.style.color = "rgba(255, 247, 97, 0.1)";}
+    if (y > 4000 && y < 5000)   {line03.style.color = "rgba(255, 247, 97, 1)";}
+    else                        {line03.style.color = "rgba(255, 247, 97, 0.1)";}
+    if (y > 5000 && y < 6000)   {line04.style.color = "rgba(255, 247, 97, 1)";}
+    else                        {line04.style.color = "rgba(255, 247, 97, 0.1)";}
+    if (y > 6000 && y < 7000)   {line05.style.color = "rgba(255, 247, 97, 1)";}
+    else                        {line05.style.color = "rgba(255, 247, 97, 0.1)";}
+    if (y > 7000 && y < 8000)   {line06.style.color = "rgba(255, 247, 97, 1)";}
+    else                        {line06.style.color = "rgba(255, 247, 97, 0.1)";}
           // Break 1000px
-    if (y > 8000 && y < 9000)   {line07.style.color = "red";}
-    else                        {line07.style.color = "black";}
-    if (y > 9000 && y < 10000)  {line08.style.color = "red";}
-    else                        {line08.style.color = "black";}
-    if (y > 10000 && y < 11000) {line09.style.color = "red";}
-    else                        {line09.style.color = "black";}
-    if (y > 11000 && y < 12000) {line10.style.color = "red";}
-    else                        {line10.style.color = "black";}
-    if (y > 12000 && y < 13000) {line11.style.color = "red";}
-    else                        {line11.style.color = "black";}
-    if (y > 13000 && y < 14000) {line12.style.color = "red";}
-    else                        {line12.style.color = "black";}
+    if (y > 8000 && y < 9000)   {line07.style.color = "rgba(255, 247, 97, 1)";}
+    else                        {line07.style.color = "rgba(255, 247, 97, 0.1)";}
+    if (y > 9000 && y < 10000)  {line08.style.color = "rgba(255, 247, 97, 1)";}
+    else                        {line08.style.color = "rgba(255, 247, 97, 0.1)";}
+    if (y > 10000 && y < 11000) {line09.style.color = "rgba(255, 247, 97, 1)";}
+    else                        {line09.style.color = "rgba(255, 247, 97, 0.1)";}
+    if (y > 11000 && y < 12000) {line10.style.color = "rgba(255, 247, 97, 1)";}
+    else                        {line10.style.color = "rgba(255, 247, 97, 0.1)";}
+    if (y > 12000 && y < 13000) {line11.style.color = "rgba(255, 247, 97, 1)";}
+    else                        {line11.style.color = "rgba(255, 247, 97, 0.1)";}
+    if (y > 13000 && y < 14000) {line12.style.color = "rgba(255, 247, 97, 1)";}
+    else                        {line12.style.color = "rgba(255, 247, 97, 0.1)";}
           // Break 1000px
-    if (y > 15000 && y < 16000) {line13.style.color = "red";}
-    else                        {line13.style.color = "black";}
-    if (y > 16000 && y < 17000) {line14.style.color = "red";}
-    else                        {line14.style.color = "black";}
-    if (y > 17000 && y < 18000) {line15.style.color = "red";}
-    else                        {line15.style.color = "black";}
-    if (y > 18000 && y < 19000) {line16.style.color = "red";}
-    else                        {line16.style.color = "black";}
-    if (y > 19000 && y < 20000) {line17.style.color = "red";}
-    else                        {line17.style.color = "black";}
-    if (y > 20000 && y < 21000) {line18.style.color = "red";}
-    else                        {line18.style.color = "black";}
+    if (y > 15000 && y < 16000) {line13.style.color = "rgba(255, 247, 97, 1)";}
+    else                        {line13.style.color = "rgba(255, 247, 97, 0.1)";}
+    if (y > 16000 && y < 17000) {line14.style.color = "rgba(255, 247, 97, 1)";}
+    else                        {line14.style.color = "rgba(255, 247, 97, 0.1)";}
+    if (y > 17000 && y < 18000) {line15.style.color = "rgba(255, 247, 97, 1)";}
+    else                        {line15.style.color = "rgba(255, 247, 97, 0.1)";}
+    if (y > 18000 && y < 19000) {line16.style.color = "rgba(255, 247, 97, 1)";}
+    else                        {line16.style.color = "rgba(255, 247, 97, 0.1)";}
+    if (y > 19000 && y < 20000) {line17.style.color = "rgba(255, 247, 97, 1)";}
+    else                        {line17.style.color = "rgba(255, 247, 97, 0.1)";}
+    if (y > 20000 && y < 21000) {line18.style.color = "rgba(255, 247, 97, 1)";}
+    else                        {line18.style.color = "rgba(255, 247, 97, 0.1)";}
 
 
 
